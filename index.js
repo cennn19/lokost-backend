557cf2d0540d71b6c264c4c8cd4ddb7fd85d4c52
cennn19 <mnugrohocandyp52@gmail.com>
2026-06-08 09:45:14 +0700

    add domain front end to railway

--- index.js
+++ index.js
@@ -22,7 +22,8 @@ app.use(cors({
         'http://127.0.0.1:5501',
         'http://localhost:5501',
         'http://localhost:3000',
-        'https://lokost-backend-production.up.railway.app'
+        'https://lokost-backend-production.up.railway.app',
+        'https://lokost.vercel.app'
     ],
     credentials: true
 }))
