ac168ee61b138ea0f7f3943ea070c369bec07314
cennn19 <mnugrohocandyp52@gmail.com>
2026-06-08 11:00:05 +0700

    fix index js

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
@@ -10,14 +9,18 @@ const budgetRoutes = require('./routes/budget');
 const logistikRoutes = require('./routes/logistik');
 const kategoriRoutes = require('./routes/kategori');
 const cookieParser = require('cookie-parser');
-app.use(express.json());
+const path = require('path');
 const rateLimit = require('express-rate-limit');
+
+app.use(express.json());
+app.use(cookieParser());
+
 const limiter = rateLimit({
     windowMs: 5 * 60 * 1000, 
     max: 10000
 });
-
 app.use(limiter);
+
 app.use(cors({
     origin: [
         'http://127.0.0.1:5501',
@@ -28,22 +31,23 @@ app.use(cors({
     ],
     credentials: true
 }))
-app.use(cookieParser());
+
 app.use('/api/user', userRoutes);
 app.use('/api/transaksi', transaksiRoutes);
 app.use('/api/budget', budgetRoutes);
 app.use('/api/logistik', logistikRoutes);
 app.use('/api/kategori', kategoriRoutes);
-const path = require('path');
+
 app.use(express.static(path.join(__dirname, '../Lokost')));
-mongoose.connect(process.env.MONGODB_URI)
+
 console.log('MONGODB_URI:', process.env.MONGODB_URI)
+mongoose.connect(process.env.MONGODB_URI)
 .then(function(){
-        console.log('Terhubung ke MongoDB!')
-    })
-    .catch(function(err){
-        console.log('Gagal konek:', err)
-    })
+    console.log('Terhubung ke MongoDB!')
+})
+.catch(function(err){
+    console.log('Gagal konek:', err)
+})
 
 app.listen(process.env.PORT || 3000, function(){
     console.log('Server berjalan!')
